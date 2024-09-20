7c1dd9b33121c78f8bbd0ee6197d3f9813b5bc7f
guntur <fxguntur04@gmail.com>
2024-09-20 11:03:35 +0700

    Fix(ui): Fixing logout modal

--- resources/views/admin/layout/partial/sidebar.blade.php
+++ resources/views/admin/layout/partial/sidebar.blade.php
@@ -171,7 +171,7 @@
                                 @csrf
                             </form>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#" id="logout-link">
+                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Logout
                             </a>
@@ -182,11 +182,24 @@
 
             </nav>
             <!-- End of Topbar -->
-            <script>
-                document.getElementById('logout-link').addEventListener('click', function(event) {
-                    event.preventDefault(); // Prevent the default link action
-                    if (confirm('Are you sure you want to log out?')) {
-                        document.getElementById('logout-form').submit(); // Submit the form
-                    }
-                });
-            </script>
+
+            <!-- Logout Modal -->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <button class="btn btn-primary" type="button"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</button>
+                </div>
+            </div>
+        </div>
+    </div>
