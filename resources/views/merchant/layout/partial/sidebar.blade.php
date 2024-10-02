4dcc7da775a7723423cc3fd27a97fd48cf90fca6
iqbalabdulmajid <iqbalabdulmajid02@gmail.com>
2024-10-02 14:01:16 +0700

    fix(bug):dashboard merchant

--- resources/views/merchant/layout/partial/sidebar.blade.php
+++ resources/views/merchant/layout/partial/sidebar.blade.php
@@ -20,7 +20,7 @@
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-            <a class="nav-link" href="{{ route('admin.dashboard') }}">
+            <a class="nav-link" href="{{ route('merchant.dashboard') }}">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Dashboard</span></a>
         </li>
